0ca3a3583602fb1529242334531cd850861c3159
Sevla <dasilvaalvesb@gmail.com>
2019-02-26 15:53:07 +0100

    add formConnection.php

---
+++ connection.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Connection</title>
+	</head>
+		<body>
+<!-- 			<?php
+				include('controlRegistration.php');
+				if (controlRegistration($_POST)['status']) {
+					include('dbPDO_members.php');
+					include('dbRegistration_addMember.php');
+					echo 'Your account has been created';
+				}
+				else {
+					include('formConnection.php');
+					echo controlRegistration($_POST)['error'];
+				}
+			?> -->
+		</body>
+</html>
---
+++ formConnection.php
@@ -0,0 +1,15 @@
+<h1>Connexion</h1>
+<form method="post">
+	<div id="pseudoDivC">
+		<label for="pseudoC">Pseudo</label>
+		<input type="text" name="pseudoC" id="pseudoC" placeholder="Your pseudonym">
+	</div>
+	<div id="passwordDivC">
+		<label for="passwordC">Password</label>
+		<input type="password" name="passwordC" id="passwordC" placeholder="Your password">
+	</div>
+	<div id="connectionDiv">
+		<input type="submit" name="connection" value="connection">
+		<input type="reset" name="reset" value="reset">
+	</div>
+</form>
