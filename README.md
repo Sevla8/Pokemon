f778d9e6b930c777948be981e62f76af2d60be2a
Sevla <dasilvaalvesb@gmail.com>
2019-03-03 13:43:07 +0100

    db request

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # Pokemon
 
+## Object
+
+WEB Application to play Pokemon 1st generation
+
 ## Fake Sendmail for Windows
 
-To be able to use php function mail in local with WAMP you must follow the steps of this tutorial (or another similar) : https://www.copier-coller.com/envoyer-des-mails-en-local-avec-wamp/
+To be able to use php function mail in local with WAMP you must follow the steps of this tutorial (or another similar) : 
+https://www.copier-coller.com/envoyer-des-mails-en-local-avec-wamp/
 
 ## DataBase Source
 
-https://github.com/bthouverez/Pokemon-BDD
-https://github.com/fanzeyi/pokemon.json
-https://www.pokebip.com/page/jeuxvideo/rbvj/version_jaune/index
+- https://github.com/bthouverez/Pokemon-BDD
+- https://github.com/fanzeyi/pokemon.json
+- https://www.pokebip.com/page/jeuxvideo/rbvj/version_jaune/index
 
-Authors :
-- Raizens
-- Sevla
+## Authors
+- Raizens ()
+- Sevla (dasilvaalvesb@gmail.com)
+
+## Dates
+
+Since Febrary 2019 
+In development
--- pokemon.sql
+++ pokemon.sql
@@ -1,15 +1,224 @@
--- Pokemon Database
--- Base de données des pokemon de la première version
--- Utilisée à des fins pédagogiques
--- Bastien Thouverez
--- Modifications et compléments apportés par Sevla
+DROP TABLE IF EXISTS type, capacite;
 
-DROP TABLE IF EXISTS pokemon, dresseur, capacite, detient_pokemon, est_type, evolu_en, possede_capacite;
+CREATE TABLE type (
+	id_type int NOT NULL,
+	name_type varchar(10) NOT NULL,
+	PRIMARY KEY(id_type)
+);
+
+INSERT INTO type(id_type, name_type) VALUES
+	(1, 'fight'),
+	(2, 'dragon'),
+	(3, 'water'),
+	(4, 'electric'),
+	(5, 'fire'),
+	(6, 'ice'),
+	(7, 'insect'),
+	(8, 'normal'),
+	(9, 'grass'),
+	(10, 'poison'),
+	(11, 'psychic'),
+	(12, 'rock'),
+	(13, 'ground'),
+	(14, 'ghost'),
+	(15, 'fly');
+
+CREATE TABLE capacity (
+	id_capa int NOT NULL,
+	name_capa varchar(25) NOT NULL,
+	type_capa int NOT NULL,
+	class_capa enum('physical', 'special'),
+	puis_capa int,
+	prec_capa int,
+	pp_capa int NOT NULL,
+	eff_sec_capa varchar(255),
+	PRIMARY KEY(id_capa),
+	FOREIGN KEY(type_capa) REFERENCES type(id_type)
+);
+
+INSERT INTO capacity VALUES 
+	(1, 'Riposte', 1, 'physical', null, 100, 20, "Inflige le double des dégâts subis par une attaque de type Normal ou Combat durant le tour, échoue sinon"),
+	(2, 'Frappe Atlas', 1, 'physical', null, 100, 20, "Inflige des dégâts égaux au niveau du lanceur"),
+	(3, 'Double Pied', 1, 'physical', 30, 100, 30, "Frappe deux fois"),
+	(4, 'Balayage', 1, 'physical', 50, 100, 20, "Peut apeurer la cible - Taux de Coups Critiques élevé"),
+	(5, 'Mawashi Geri', 1, 'physical', 60, 85, 15, "Peut apeurer la cible"),
+	(6, 'Pied Sauté', 1, 'physical', 70, 95, 25, "Enlève 1 PV si l'attaque échoue"),
+	(7, 'Sacrifice', 1, 'physical', 80, 80, 25, "Blesse le lanceur"),
+	(8, 'Pied Voltige', 1, 'physical', 85, 90, 20, "Enlève 1 PV si l'attaque échoue"),
+	(9, 'Draco-Rage', 2, 'special', null, 100, 10, "Inflige toujours 40 PV de dégâts"),
+	(10, 'Repli', 3, null, null, 100, 40, "Augmente la défense du lanceur"),
+	(11, 'Écume', 3, "special", 20, 100, 30, "Peut baisser la vitesse de la cible"),
+	(12, 'Claquoir', 3, 'special', 35, 75, 10, "Attaque continue sur deux à cinq tours"),
+	(13, 'Pistolet à O', 3, 'special', 40, 100, 25, null),
+	(14, "Bulles d'O", 3, 'special', 65, 100, 20, 'Peut baisser la vitesse de la cible'),
+	(15, 'Cascade', 3, 'special', 80, 100, 15, null),
+	(16, 'Pince-Masse', 3, 'special', 90, 85, 10, 'Taux de Coups Critiques élevé'),
+	(17, 'Surf', 3, 'special', 95, 100, 15, null),
+	(18, 'Hydrocanon', 3, 'special', 120, 80, 5, null),
+	(19, 'Cage-Éclair', 4, null, null, null, 100, 20, 'Paralyse la cible'),
+	(20, 'Éclair', 4, 'special', 40, 100, 30, 'Peut paralyser la cible'),
+	(21, 'Poing-Éclair', 4, 'special', 75, 100, 15, 'Peut paralyser la cible'),
+	(22, 'Tonnerre', 4, 'special', 95, 100, 15, 'Peut paralyser la cible'),
+	(23, 'Fatal-Foudre', 4, 'special', 120, 70, 5, 'Peut paralyser la cible'),
+	(24, 'DanseFlamme' , 5, 'special', 15, 70, 15, "Attaque continue sur deux à cinq tours"),
+	(25, 'Flammèche', 5, 'special', 40, 100, 25, 'Peut brûler la cible'),
+	(26, 'Poing de Feu', 5, 'special', 75, 100, 15, 'Peut brûler la cible'),
+	(27, 'Lance-Flamme', 5, 'special', 95, 100, 15, 'Peut brûler la cible'),
+	(28, 'Déflagration', 5, 'special', 120, 85, 5, 'Peut brûler la cible'),
+	(29, 'Brume', 6, null, null, null, 30, "Empêche toute variation de stat provoquée par l'adversaire"),
+	(30, 'Buée Noire', 6, null, null, null, 30, "Annule toute variation de stat, la confusion, les effets de Puissance, Vampigraine, Mur Lumière et Protection"),
+	(31, 'Onde Boréale', 6, 'special', 65, 100, 20, "Peut baisser l'attaque la cible"),
+	(32, 'Poing-Glace', 6, 'special', 75, 100, 15, "Peut geler la cible"),
+	(33, 'Laser Glace', 6, 'special', 95, 100, 10, "Peut geler la cible"),
+	(34, 'Blizzard', 6, 'special', 120, 90, 5, "Peut geler la cible"),
+	(35, 'Sécrétion', 7, null, null, 95, 40, "Diminue la vitesse de la cible"),
+	(36, 'Dard Nuée', 7, 'physical', 14, 85, 20, "Attaque deux à cinq fois"),
+	(37, 'Vampirisme', 7, 'physical', 20, 100, 15, "Restaure un nombre de PV au lanceur égal à la moitié des dégâts infligés à la cible"),
+	(38, 'Double-Dard', 7, 'physical', 25, 100, 20, "Attaque deux fois - Peut empoisoner"),
+	(39, 'Adaptation', 8, null, null, null, 30, "Le lanceur devient du même type que l'adversaire"),
+	(40, 'Affûtage', 8, null, null, null, 30, "Augmente l'attaque du lanceur"),
+	(41, 'Armure', 8, null, null, null, 30, "Augmente la défense du lanceur"),
+	(42, 'Berceuse', 8, null, null, 55, 15, "Endort la cible"),
+	(43, "Boul'Armure", 8, null, null, null, 40, "Augmente la défense du lanceur"),
+	(44, "Brouillard", 8, null, null, 100, 20, "Baisse la précision de la cible"), 
+	(45, 'Clonage', 8, null, null, null, 10, "Crée un clone qui prend les dommages à la place du lanceur, enlève 25% des PV du lanceur"),
+	(46, 'Copie', , 8, null, null, null, 10, "Apprend une des attaques de l'advresaire au hasard jusqu'à la fin du combat"), 
+	(47, 'Croissance', , 8, null, null, null, 40, "Augmente le spécial du lanceur"), 
+	(48, "Cyclone", 8, null, null, 100, 20, "Met fin au combat contre un Pokémon sauvage, échoue contre un dresseur"),
+	(49, "Danse-Lames", 8, null, null, null, 30, "Augmente l'attaque du lanceur de deux niveaux"),
+	(50, "E-Coque", 8, null, null, null, 10, "Restaure jusqu'à la moitié des PV du lanceur"), 
+	(51, "Entrave", 8, null, null, 55, 20, "Supprime durant un à huit tour une attaque au hasard de la cible"), 
+	(52, "Flash", 8, null, null, 70, 20, "Baisse la précision de la cible"), 
+	(53, "Grincement", 8, null, null, 85, 40, "Baisse la défense de l'adversaire de deux niveaux"), 
+	(54, "Grobisou"	, 8, null, null, 75, 10, "Endort la cible"), 
+	(55, "Groz'Yeux", 8, null, null, 100, 30, "Baisse la défense de la cible"),
+	(56, "Hurlement", 8, null, null, 100, 20, "Met fin au combat contre un Pokémon sauvage, échoue contre un dresseur"),
+	(57, "Intimidation", 8, null, null, 75, 30, "Paralyse la cible"),
+	(58, "Jet de Sable", 8, null, null, 100, 15, "Baisse la précision de la cible"),
+	(59, "Lilliput", 8, null, null, null, 20, "Augmente l'esquive du lanceur"), 
+	(60, "Métronome", 8, null, null, null, 10, "Exécute une attaque prise au hasard dans cette liste"), 
+	(61, "Mimi-Queue", 8, null, null, 100, 30, "Baisse la défense de la cible"), 
+	(62, "Morphing", 8, null, null, null, 10, "Transforme le lanceur en le Pokémon adverse"), 
+	(63, "Puissance", 8, null, null, null, 30, "Augmente le taux de Coups Critiques du lanceur"), 
+	(64, "Reflet", 8, null, null, null, 15, "Augmente l'esquive du lanceur"), 
+	(65, "Rugissement", 8, null, null, null, 40, "Baisse l'attaque de la cible"), 
+	(66, "Soin", 8, null, null, null, 20, "Restaure jusqu'à la moitié des PV du lanceur"), 
+	(67, "Trempette", 8, null, null, null, 40, null),
+	(68, "Ultrason", 8, null, null, 55	20, "Rend la cible confuse"), 
+	(69, "Croc Fatal", 8, 'physical', null, 90, 10, "Enlève 50% des PV restants de la cible"),
+	(70, "Patience", 8, 'physical', null, null, 10, "Immobilise le lanceur deux ou trois tours, puis inflige à l'adversaire le double des dégâts subis pendant cette période"), 
+	(71, "Sonicboom", 8, 'physical', null, 90, 20, "Inflige toujours 20 PV de dégâts"), 
+	(72, "Empal'Korne", 8, 'physical', null, 30, 5, "Met l'adversaire KO, échoue s'il a une vitesse supérieure à celle du lanceur"), 
+	(73, "Guillotine", 8, 'physical', null, 30, 5, "Met l'adversaire KO, échoue s'il a une vitesse supérieure à celle du lanceur"), 
+	(74, "Constriction", 8, 'physical',	10, 100, 35, "Peut baisser la vitesse de la cible"), 
+	(75, "Étreinte", 8, 'physical',	15, 85, 20, "Attaque continue sur deux à cinq tours"), 
+	(76, "Furie", 8, 'physical', 15, 85, 20, "Attaque deux à cinq fois"), 
+	(77, "Ligotage", 8, 'physical', 15, 90, 20, "Attaque continue sur deux à cinq tours"), 
+	(78, "Pilonnage", 8, 'physical', 15, 85, 20, "Attaque deux à cinq fois"), 
+	(79, "Torgnoles", 8, 'physical', 15, 85, 10, "Attaque deux à cinq fois"), 
+	(80, "Combo-Griffe", 8, 'physical',	18, 80, 15, "Attaque deux à cinq fois"), 
+	(81, "Poing Comète", 8, 'physical',	18, 85, 15, "Attaque deux à cinq fois"), 
+	(82, "Frénésie", 8, 'physical', 20, 100, 20, "Attaque sans fin, provoque l'augmentation de l'attaque du lanceur à chaque fois qu'il subit des dégâts"), 
+	(83, "Picanon", 8, , 'physical', 20, 100, 15, "Attaque deux à cinq fois"), 
+	(84, "Charge", 8, 'physical', 35, 95, 35, null),
+	(85, "Tornade", 8, 'physical', 35, 100, 40, null),
+	(86, "Écras'Face", 8, 'physical', 40, 100, 35, null),
+	(87, "Griffe", 8, 'physical', 40, 100, 35, null),
+	(88, "Jackpot", 8, 'physical', 40, 100, 20, "Fait gagner un peu d'argent à la fin des combats"), 
+	(89, "Vive-Attaque", 8, 'physical',	40, 100, 30, "Attaque en premier"), 
+	(90, "Coupe", 8, 'physical', 50, 95, 30, null), 
+	(91, "Poing Karaté", 8, 'physical',	50, 100, 25, "Taux de Coups Critiques élevé"), 
+	(92, "Lutte", 8, 'physical', 50, 100, null, "Blesse le lanceur"), 
+	(93, "Force Poigne", 8, 'physical', 55, 100, 30, null), 
+	(94, "Météores", 8, 'physical', 60, null, 20, "N'échoue jamais"), 
+	(95, "Morsure", 8, 'physical', 60, 100, 25, "Peut apeurer la cible"), 
+	(96, "Écrasement", 8, 'physical', 65, 100, 20, "Peut apeurer la cible"), 
+	(97, "Koud'Korne", 8, 'physical', 65, 100, 25, null), 
+	(98, "Coud'Boule", 8, 'physical', 70, 100, 15, "Peut apeurer la cible"), 
+	(99, "Tranche", 8, 'physical', 70, 100, 20, "Taux de Coups Critiques élevé"), 
+	(100, "Uppercut", 8, 'physical', 70, 100, 10, null), 
+	(101, "Coupe-Vent", 8, 'physical', 80, 100, 10, "Attaque en deux tours, n'agit pas au premier"), 
+	(102, "Croc de Mort", 8, 'physical', 80, 90, 15, "Peut apeurer la cible"), 
+	(103, "Force", 8, 'physical', 80, 100, 15, null), 
+	(104, "Souplesse", 8, 'physical', 80, 75, 20, null), 
+	(105, "Triplattaque", 8, 'physical', 80, 100, 10, null), 
+	(106, "Ultimapoing", 8, 'physical', 80, 85, 20, null), 
+	(107, "Plaquage", 8, 'physical', 85, 100, 15, "Peut paralyser la cible"), 
+	(108, "Bélier", 8, 'physical', 90, 85, 20, "Blesse le lanceur"), 
+	(109, "Mania", 8, 'physical', 90, 100, 20, "Attaque continue sur deux ou trois tours, le lanceur devient confus par la suite"), 
+	(110, "Bomb'Œuf", 8, 'physical', 100, 75, 16, null), 
+	(111, "Coud'Krâne", 8, 'physical', 100, 100, 15, "Attaque en deux tours, n'agit pas au premier"), 
+	(112, "Damoclès", 8, 'physical', 100, 100, 15, "Blesse le lanceur"), 
+	(113, "Ultimawashi", 8, 'physical', 120, 75, 5, null), 
+	(114, "Destruction", 8, 'physical', 130, 100, 5, "Lanceur KO après l'attaque, ne tient compte que de la moitié de la défense adverse, ce qui équivaut à une puissance réelle de 260"), 
+	(115, "Ultralaser", 8, 'physical', 150, 90, 5, "Lanceur immobilisé au tour suivant"), 
+	(116, "Explosion", 8, 'physical', 170, 100, 5, "Lanceur KO après l'attaque, ne tient compte que de la moitié dela défense adverse, ce qui équivaut à une puissance réelle de 340"), 
+	(Para-Spore	Type plante 3G	- 	- 	75	30	Paralyse la cible
+	(Poudre Dodo	Type plante 3G	- 	- 	75	15	Endort la cible
+	(Spore	Type plante 3G	- 	- 	100	15	Endort la cible
+	(Vampigraine	Type plante 3G	- 	- 	90	10	Draîne à chaque tour des PV de la cible vers le lanceur
+	(Vol-Vie	Type plante 3G	, 'special',	20	100	20	Restaure un nombre de PV au lanceur égal
+	(à la moitié des dégâts infligés à la cible
+	(Fouet Lianes	Type plante 3G	, 'special',	35	100	10	null
+	(Méga-Sangsue	Type plante 3G	, 'special',	40	100	10	Restaure un nombre de PV au lanceur égal
+	(à la moitié des dégâts infligés à la cible
+	(Tranch'Herbe	Type plante 3G	, 'special',	55	95	25	Taux de Coups Critiques élevé
+	(Danse-Fleur	Type plante 3G	, 'special',	70	100	20	Attaque continue sur deux ou trois tour, le
+	(lanceur devient confus par la suite
+	(Lance-Soleil	Type plante 3G	, 'special',	120	100	10	Attaque en deux tours, n'agit pas au premier
+	(Acidarmure	Type poison 3G	- 	- 	- 	40	Augmente la défense du lanceur de deux niveaux
+	(Gaz Toxik	Type poison 3G	- 	- 	55	40	Empoisonne la cible
+	(Poudre Toxik	Type poison 3G	- 	- 	75	35	Empoisonne la cible
+	(Toxik	Type poison 3G	- 	- 	85	10	Empoisonne gravement la cible
+	(Dard-Venin	Type poison 3G	, 'physical',	15	100	35	Peut empoisonner la cible
+	(Purédpois	Type poison 3G	, 'physical',	20	70	20	Peut empoisonner la cible
+	(Acide	Type poison 3G	, 'physical',	40	100	30	Peut baisser la défense de la cible
+	(Détritus	Type poison 3G	, 'physical',	65	100	20	Peut empoisonner la cible
+	(Amnésie	Type psy 3G	- 	- 	- 	20	Augmente le spécial du lanceur de deux niveaux
+	(Bouclier	Type psy 3G	- 	- 	- 	30	Augmente la défense du lanceur de deux niveaux
+	(Hâte	Type psy 3G	- 	- 	- 	30	Augmente la vitesse du lanceur de deux niveaux
+	(Hypnose	Type psy 3G	- 	- 	60	20	Endort la cible
+	(Mur Lumière	Type psy 3G	- 	- 	- 	30	Augmente la résistance du lanceur aux attaques , 'special',s
+	(Protection	Type psy 3G	- 	- 	- 	20	Augmente la résistance du lanceur aux attaques , 'physical',s
+	(Repos	Type psy 3G	- 	- 	- 	10	Restaure tous les PV du lanceur ainsi que son statut,
+	(le lanceur est immobilisé pendant deux tours
+	(Télékinésie	Type psy 3G	- 	- 	80	15	Baisse la précision de la cible
+	(Téléport	Type psy 3G	- 	- 	- 	20	Met fin au combat contre un Pokémon sauvage,
+	(échoue contre un dresseur
+	(Yoga	Type psy 3G	- 	- 	- 	40	Augmente l'attaque du lanceur
+	(Vague Psy	Type psy 3G	, 'special',	- 	80	15	Inflige des dégâts variables
+	(Choc Mental	Type psy 3G	, 'special',	50	100	25	Peut rendre la cible confuse
+	(Rafale Psy	Type psy 3G	, 'special',	65	100	20	Peut rendre la cible confuse
+	(Psyko	Type psy 3G	, 'special',	90	100	10	Peut baisser le spécial de la cible
+	(Dévorêve	Type psy 3G	, 'special',	100	100	15	Restaure un nombre de PV au lanceur égal à la moitié des dégâts
+	(infligés à la cible, ne fonctionne que si la cible est endormie
+	(Jet-Pierres	Type roche 3G	, 'physical',	50	65	15	null
+	(Éboulement	Type roche 3G	, 'physical',	75	90	10	Peut apeurer la cible
+	(Abîme	Type sol 3G	, 'physical',	- 	30	5	Met l'adversaire KO, échoue s'il a une vitesse
+	(supérieure à celle du lanceur
+	(Osmerang	Type sol 3G	, 'physical',	50	90	10	Attaque deux fois
+	(Massd'Os	Type sol 3G	, 'physical',	65	85	10	Peut apeurer la cible
+	(Séisme	Type sol 3G	, 'physical',	100	100	10	null
+	(Tunnel	Type sol 3G	, 'physical',	100	100	10	Attaque en deux tours, le lanceur ne peut être atteint
+	(que par Météores et Patience au premier tour
+	(Onde Folie	Type spectre 3G	- 	- 	100	10	Rend la cible confuse
+	(Ténèbres	Type spectre 3G	, 'physical',	- 	100	15	Inflige des dégâts égaux au niveau du lanceur
+	(Léchouille	Type spectre 3G	, 'physical',	20	100	30	Peut paralyser la cible
+	(Mimique	Type vol 3G	- 	- 	- 	20	Le lanceur utilise la dernière attaque lancée par l'adversaire
+	(Cru-Aile	Type vol 3G	, 'physical',	35	100	35	null
+	(Picpic	Type vol 3G	, 'physical',	35	100	35	null
+	(Vol	Type vol 3G	, 'physical',	70	100	15	Attaque en deux tours, le lanceur ne peut être atteint
+	(que par Météores et Patience au premier tour
+	(Bec Vrille	Type vol 3G	, 'physical',	80	100	20	null
+	(165, 'Piqué', 15, 'physical', 140, 90, 5, "Attaque en deux tours, n'agit pas au premier");
+
 
 CREATE TABLE pokemon (
 	id_pok int NOT NULL,
 	nom_pok varchar(25) NOT NULL,
-	PRIMARY KEY(id_pok)
+	id_type varchar(25) NOT NULL,
+	PRIMARY KEY(id_pok),
+	FOREIGN KEY(id_type) REFERENCES type(id_type)
 );
 
 -- Table evolueEn
@@ -64,14 +273,8 @@ CREATE TABLE capacite (
 
 CREATE TABLE possede_capacite (
 	id_pok int NOT NULL, 
-	id_capa_1 varchar(25) NOT NULL,  
-	id_capa_2 varchar(25) NOT NULL,
-	id_capa_3 varchar(25) NOT NULL,
-	id_capa_4 varchar(25) NOT NULL,
-	FOREIGN KEY(id_capa_1) REFERENCES capacite(id_capa),
-	FOREIGN KEY(id_capa_2) REFERENCES capacite(id_capa),
-	FOREIGN KEY(id_capa_3) REFERENCES capacite(id_capa),
-	FOREIGN KEY(id_capa_4) REFERENCES capacite(id_capa),
+	id_capa varchar(25) NOT NULL,
+	FOREIGN KEY(id_capa) REFERENCES capacite(id_capa),
 	FOREIGN KEY(id_pok) REFERENCES pokemon(id_pok)
 );
 
